05d1e2dc4ff4439060407790115f839ac1426098
ilia A <cs.ia1387@gmail.com>
2023-03-25 14:28:07 +1100

    init

---
+++ src/main/java/au/com/daisbuild/daisbuild/DaisbuildApplication.java
@@ -0,0 +1,13 @@
+package au.com.daisbuild.daisbuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaisbuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaisbuildApplication.class, args);
+	}
+
+}
---
+++ src/test/java/au/com/daisbuild/daisbuild/DaisbuildApplicationTests.java
@@ -0,0 +1,13 @@
+package au.com.daisbuild.daisbuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaisbuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
