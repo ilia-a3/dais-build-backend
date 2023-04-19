51fddc7b4fdd1ffa459e322ac575d9346b2dd5d2
ilia A <cs.ia1387@gmail.com>
2023-04-19 14:09:13 +1000

    deployment update

--- src/main/java/au/com/daisbuild/daisbuild/DaisbuildApplication.java
+++ src/main/java/au/com/daisbuild/daisbuild/DaisbuildApplication.java
@@ -3,10 +3,11 @@ package au.com.daisbuild.daisbuild;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class DaisbuildApplication {
+public class DaisbuildApplication extends SpringBootServletInitializer {
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
